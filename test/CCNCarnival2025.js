66b23e72823ada6c971a0375d0b6c9cf7d9dd600
dddariu <dariustsz06@gmail.com>
2025-08-10 19:13:59 +0800

    updated

--- test/CCNCarnival2025.js
+++ test/CCNCarnival2025.js
@@ -25,18 +25,14 @@ describe("CCNCarnival2025", function () {
   });
 
   it("Owner can issue a refund to a buyer", async () => {
-    // Register a stall
     await ccn.registerStall(0);
 
-    // Buyer makes a payment
     await ccn.connect(buyer).makePayment(1, { value: ethers.parseEther("1") });
 
-    // Refund the buyer
     await expect(ccn.issueRefund(1, buyer.address))
       .to.emit(ccn, "RefundIssued")
       .withArgs(1, buyer.address, ethers.parseEther("1"));
 
-    // Check balances updated
     const stall = await ccn.stalls(1);
     expect(stall.balance).to.equal(0);
 
@@ -45,18 +41,14 @@ describe("CCNCarnival2025", function () {
   });
 
   it("Owner can withdraw stall funds", async () => {
-    // Register a stall
     await ccn.registerStall(0);
 
-    // Buyer makes payment
     await ccn.connect(buyer).makePayment(1, { value: ethers.parseEther("2") });
 
-    // Withdraw
     await expect(ccn.withdrawFunds(1))
       .to.emit(ccn, "FundsWithdrawn")
       .withArgs(1, owner.address, ethers.parseEther("2"));
 
-    // Stall should now have 0 balance and withdrawn = true
     const stall = await ccn.stalls(1);
     expect(stall.balance).to.equal(0);
     expect(stall.withdrawn).to.equal(true);
